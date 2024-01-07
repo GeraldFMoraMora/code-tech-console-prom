97fb57405b2bc6a1f3651546e60e7c18df8ad2e6
GeraldFMoraMora <geraldmoramora@gmail.com>
2024-01-07 12:00:46 -0600

    agregado menu y estructura de la aplicacion de consola

--- src/App.java
+++ src/App.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
+import model.service.CustomerService;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        CustomerService customerService = new CustomerService();
+
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("\nMenú de navegación:");
+            System.out.println("1. Crear cliente");
+            System.out.println("2. Eliminar cliente");
+            System.out.println("3. Actualizar cliente");
+            System.out.println("4. Obtener cliente por ID");
+            System.out.println("5. Listar clientes");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                System.out.println("Opción 1: Crear cliente");
+                    
+                    break;
+                case 2:
+                System.out.println("Opción 2: Eliminar cliente");
+                    
+                    break;
+                case 3:
+                System.out.println("Opción 3: Actualizar cliente");
+                    
+                    break;
+                case 4:
+                System.out.println("Opción 4: Obtener cliente por ID");
+                    
+                    break;
+                case 5:
+                System.out.println("Opción 5: Listar clientes");
+                    
+                    break;
+                case 0:
+                System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    
+                    break;
+                default:
+                    break;
+            }
+
+        } while (option !=0);
     }
 }
---
+++ src/model/entity/Customer.java
@@ -0,0 +1,45 @@
+package model.entity;
+
+import java.util.Date;
+
+public class Customer {
+
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private Date birthdate;
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public Date getBirthdate() {
+        return birthdate;
+    }
+    
+}
